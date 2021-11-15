2728bc489c28af025e6c56ad0f4b209f6f040ac5
PillariPavankumar <vijayalakshmi@Pavankumar.com>
2021-11-15 09:50:43 +0530

    hello

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron-Man' },
+  { id: 2, name: 'Spider-Man' },
+  { id: 3, name: 'Captain-America' },
+  { id: 4, name: 'Hulk' },
+  { id: 5, name: 'Dr.Strange' },
+  { id: 6, name: 'Thor' },
+  { id: 7, name: 'Black-Panther' },
+  { id: 8, name: 'Natasha' },
+  { id: 9, name: 'Vision' },
+  { id: 10, name: 'Thanos' }
+];
